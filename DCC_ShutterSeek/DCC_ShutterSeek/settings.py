aa41b7eff78e837fce32698bedf05c83680aa39b
Manav Sengupta <65215108+manavsengupta@users.noreply.github.com>
2024-07-13 22:19:54 +0530

    Update settings.py

--- DCC_ShutterSeek/DCC_ShutterSeek/settings.py
+++ DCC_ShutterSeek/DCC_ShutterSeek/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mzmz@hq=n-&69qk3*_(9zlmeilu52^76a0^c87ilp7r#+23^mh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
